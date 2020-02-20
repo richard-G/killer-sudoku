60d990c2c85238bfe2210d06ba9c9f929b37e0c5
Richard Glass <ricardoglass@gmail.com>
2020-02-20 12:48:13 +0000

    code cleanup

--- script.py
+++ script.py
@@ -18,10 +18,22 @@ grid = np.array([
     [0, 0, 0, 0, 8, 0, 0, 7, 9]
 ])
 
+# grid = np.array([
+#     [5, 3, 0, 0, 7, 0, 0, 0, 0],
+#     [6, 0, 0, 1, 9, 5, 0, 0, 0],
+#     [0, 9, 8, 0, 0, 0, 0, 6, 0],
+#     [8, 0, 0, 0, 0, 0, 0, 0, 0],
+#     [4, 0, 0, 8, 0, 0, 0, 0, 1],
+#     [7, 0, 0, 0, 2, 0, 0, 8, 6],
+#     [0, 6, 0, 0, 0, 0, 2, 4, 0],
+#     [0, 0, 0, 4, 1, 9, 0, 0, 5],
+#     [0, 0, 0, 0, 8, 0, 0, 7, 9]
+# ])
+
 
 # function to display the grid
 def display_grid():
-    global grid
+    # global grid
     for row in grid:
         print(row)
 
@@ -29,7 +41,7 @@ def display_grid():
 # returns true if a given number is allowed at a given position, on the global grid
 # position entered as a 2-tuple, (row, column)
 def is_valid(number, position):
-    global grid
+    # global grid
     row_index, col = position
 
     # check row
@@ -41,23 +53,6 @@ def is_valid(number, position):
         if row[col] == number:
             return False
 
-    # check this
-    # if number in grid[:][col]:
-    #     return False
-
-    # # get subgrid coords
-    # for i in range(0, 12, 3):
-    #     if row_index < i:
-    #         for j in range(0, 12, 3):
-    #             if col < j:
-    #                 subgrid_i, subgrid_j = (i//3, j//3)
-    #
-    # # check subgrid
-    # for i in range(subgrid_i * 3, subgrid_i + 3):
-    #     for j in range(subgrid_j * 3, subgrid_j + 3):
-    #         if grid[i][j] == number:
-    #             return False
-
     i0 = (row_index // 3) * 3
     j0 = (col // 3) * 3
 
@@ -66,14 +61,12 @@ def is_valid(number, position):
             if grid[i0 + i][j0 + j] == number:
                 return False
 
-    # could clean up subgrid mixing the above 2, low row_index // 3 perhaps?
-
     return True
 
 
 # recursive function to be called to solve the puzzle.
 def solve_puzzle():
-    global grid
+    # global grid
     for i in range(9):
         for j in range(9):
             if grid[i][j] == 0:
@@ -83,7 +76,6 @@ def solve_puzzle():
                         solve_puzzle()
                         grid[i][j] = 0
                 return
-
 
     print('end of program.. solved puzzle:')
     display_grid()
